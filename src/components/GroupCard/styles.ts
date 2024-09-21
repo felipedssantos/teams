bf65ed048566db5ee974d65db811abc614e6bd77
felipedumontt@hotmail.com <Felipe Dumont>
2024-09-21 17:27:54 -0300

    Feat: New Group screen and components

--- src/components/GroupCard/styles.ts
+++ src/components/GroupCard/styles.ts
@@ -1,6 +1,7 @@
+import styled, { css } from "styled-components/native";
+
 import { TouchableOpacity } from "react-native";
 import { UsersThree } from "phosphor-react-native";
-import styled from "styled-components/native";
 
 export const Container = styled(TouchableOpacity)`
   width: 100%;
@@ -16,9 +17,11 @@ export const Container = styled(TouchableOpacity)`
 `;
 
 export const Title = styled.Text`
-  font-size: ${({ theme }) => theme.FONT_SIZE.MD};
-  font-family: ${({ theme }) => theme.FONT_FAMILY.REGULAR};
-  color: ${({ theme }) => theme.COLORS.GRAY_200};
+  ${({ theme }) => css`
+    font-size: ${theme.FONT_SIZE.MD};
+    font-family: ${theme.FONT_FAMILY.REGULAR};
+    color: ${theme.COLORS.GRAY_200};
+  `};
 `;
 
 export const Icon = styled(UsersThree).attrs(({ theme }) => ({
--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,10 +1,27 @@
-import { Container } from "./styles";
+import { Container, Content, Icon } from "./styles";
+
+import { Button } from "@components/Button";
 import { Header } from "@components/Header";
+import { Highlight } from "@components/Highlight";
+import { Input } from "@components/Input";
 
 export function NewGroup() {
   return (
     <Container>
       <Header showBackButton />
+
+      <Content>
+        <Icon />
+
+        <Highlight
+          title="Nova Turma"
+          subtitle="Crie a turma para adicionar as pessoas"
+        />
+
+        <Input placeholder="Nome da turma" />
+
+        <Button title="Criar turma" style={{ marginTop: 20 }} />
+      </Content>
     </Container>
   );
 }
